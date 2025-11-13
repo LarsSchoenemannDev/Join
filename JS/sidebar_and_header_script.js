d7cb4201a60469a0ea8a0b1a5860588b66071360
Dennis Köklü <dennis.koeklue@gmail.com>
2025-11-13 11:14:55 +0100

    Changes and added on Data ; privacy-policy.css, css/sidebar-responsiv.css, html/privacy-policy.html, JS/sidebar_and_header_script.js, desktop_templates.html

--- JS/sidebar_and_header_script.js
+++ JS/sidebar_and_header_script.js
@@ -1,10 +1,19 @@
 
 //Hier bitte alle globale Variablen setzen
 let user_icon = document.getElementById('user-icon')
-
+let switch_help = document.getElementById('switch_help')
 
 function tooglemenu() {
     let msg = document.getElementById('dropDown-menu')
     msg.classList.toggle('show')
 }
 
+function helpOnResizeSwitch() {
+if (window.innerWidth <= 768 ) {
+    switch_help.classList.remove('hide');
+}else{
+    switch_help.classList.add('hide');
+}
+}
+
+window.onresize = helpOnResizeSwitch;
