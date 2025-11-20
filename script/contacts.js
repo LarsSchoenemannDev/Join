78164aefe36b11a77360d1481e6a30fed10cc1f6
Niloufar <153923465+Niloufar88@users.noreply.github.com>
2025-11-20 22:11:08 +0530

    summary page for logged-in user

--- script/contacts.js
+++ script/contacts.js
@@ -12,7 +12,7 @@ const phoneInput = document.getElementById('phoneInput');
 const createMessage = document.querySelector('.popup-message');
 const editContactPopup = document.querySelector('.edit-contact-popup');
 
-let colors = ["rgba(255, 122, 0, 1)", "rgba(255, 94, 179, 1)", "rgba(110, 82, 255, 1)", "rgba(147, 39, 255, 1)", "rgba(0, 190, 232, 1)", "rgba(31, 215, 193, 1)", "rgba(255, 116, 94, 1)", "rgba(255, 163, 94, 1)", "rgba(252, 113, 255, 1)", "rgba(255, 199, 1, 1)", "rgba(0, 56, 255, 1)", "rgba(195, 255, 43, 1)", "rgba(255, 230, 43, 1)", "rgba(255, 70, 70, 1)", "rgba(255, 187, 43, 1)"];
+
 
 
 // Initialize app: load data from Firebase, then create contact list
@@ -198,7 +198,6 @@ function closePopupOverlay() {
     setTimeout(() => {
         addContactPopup.classList.add('d-none');
     }, 500);
-
     clearInputFields();
 }
 
