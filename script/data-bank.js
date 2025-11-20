c8fafc68604c4b2746a14eaaf3844d7da867a075
Niloufar <153923465+Niloufar88@users.noreply.github.com>
2025-11-20 21:56:46 +0530

    var colors saved in data-bank for global usage

--- script/data-bank.js
+++ script/data-bank.js
@@ -1,61 +1,64 @@
-let contacts = [{
-        id: 1,
-        name: "Anton Mayer",
-        email: "antonm@gmail.com",
-        phone: "+49 152 456789"
-    },
-    {
-        id: 2,
-        name: "Anja Schulz",
-        email: "schulz@hotmail.com",
-        phone: "+49 152 789877"
-    },
-    {
-        id: 3,
-        name: "Benedikt Ziegler",
-        email: "benedikt@gmail.com",
-        phone: "+49 176 978687"
-    },
-    {
-        id: 4,
-        name: "David Eisenberg",
-        email: "davidberg@gmail.com",
-        phone: "+49 152 6597632"
-    },
-    {
-        id: 5,
-        name: "Eva Fischer",
-        email: "eva@gmail.com",
-        phone: "+49 152 6546466"
-    },
-    {
-        id: 6,
-        name: "Emmanuel Mauer",
-        email: "emmanuel@hotmail.com",
-        phone: "+49 176 6554653"
-    },
-    {
-        id: 7,
-        name: "Marcel Bauer",
-        email: "marcel.bauer@hotmail.com",
-        phone: "+49 176 65464500"
-    },
-    {
-        id: 8,
-        name: "Tatjana Wolf",
-        email: "tatjana.wolf@hotmail.com",
-        phone: "+49 111 23546534"
-    },
-    {
-        id: 9,
-        name: "Paula Diomond",
-        email: "paula.diomond@hotmail.com",
-        phone: "+49 176 1112236"
-    },
-    {
-        id: 10,
-        name: "Victoria Holzapfel",
-        email: "victoria.holzapfel@gmail.com",
-        phone: "+49 152 3366647"
-    }
-];
+let colors = ["rgba(255, 122, 0, 1)", "rgba(255, 94, 179, 1)", "rgba(110, 82, 255, 1)", "rgba(147, 39, 255, 1)", "rgba(0, 190, 232, 1)", "rgba(31, 215, 193, 1)", "rgba(255, 116, 94, 1)", "rgba(255, 163, 94, 1)", "rgba(252, 113, 255, 1)", "rgba(255, 199, 1, 1)", "rgba(0, 56, 255, 1)", "rgba(195, 255, 43, 1)", "rgba(255, 230, 43, 1)", "rgba(255, 70, 70, 1)", "rgba(255, 187, 43, 1)"];
+
+
+// let contacts = [{
+//         id: 1,
+//         name: "Anton Mayer",
+//         email: "antonm@gmail.com",
+//         phone: "+49 152 456789"
+//     },
+//     {
+//         id: 2,
+//         name: "Anja Schulz",
+//         email: "schulz@hotmail.com",
+//         phone: "+49 152 789877"
+//     },
+//     {
+//         id: 3,
+//         name: "Benedikt Ziegler",
+//         email: "benedikt@gmail.com",
+//         phone: "+49 176 978687"
+//     },
+//     {
+//         id: 4,
+//         name: "David Eisenberg",
+//         email: "davidberg@gmail.com",
+//         phone: "+49 152 6597632"
+//     },
+//     {
+//         id: 5,
+//         name: "Eva Fischer",
+//         email: "eva@gmail.com",
+//         phone: "+49 152 6546466"
+//     },
+//     {
+//         id: 6,
+//         name: "Emmanuel Mauer",
+//         email: "emmanuel@hotmail.com",
+//         phone: "+49 176 6554653"
+//     },
+//     {
+//         id: 7,
+//         name: "Marcel Bauer",
+//         email: "marcel.bauer@hotmail.com",
+//         phone: "+49 176 65464500"
+//     },
+//     {
+//         id: 8,
+//         name: "Tatjana Wolf",
+//         email: "tatjana.wolf@hotmail.com",
+//         phone: "+49 111 23546534"
+//     },
+//     {
+//         id: 9,
+//         name: "Paula Diomond",
+//         email: "paula.diomond@hotmail.com",
+//         phone: "+49 176 1112236"
+//     },
+//     {
+//         id: 10,
+//         name: "Victoria Holzapfel",
+//         email: "victoria.holzapfel@gmail.com",
+//         phone: "+49 152 3366647"
+//     }
+// ];
