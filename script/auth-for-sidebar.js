f22daa734145dc1e32e606f69d45fe813f394481
Dennis Köklü <dennis.koeklue@gmail.com>
2025-12-09 10:35:12 +0100

    added und changes on auth-side, html changes img

--- script/auth-for-sidebar.js
+++ script/auth-for-sidebar.js
@@ -1,11 +1,21 @@
 let normalUserLinks = document.getElementById('user-links');
+let allUserLinks = normalUserLinks.querySelectorAll("a");
 let notUserLoginLinks = document.getElementById('not-user-links');
-let headerIcons = document.getElementById('user-icon');
+let headerIcons = document.getElementById('user-icons');
 const userStatus = sessionStorage.getItem('userStatus');
 
 if (!userStatus || (userStatus !== 'guest' && userStatus !== 'loggedIn')) {
     normalUserLinks.classList.add('hide')
     notUserLoginLinks.classList.remove('hide')
+    headerIcons.classList.add('hide')
+    disableLinks()
+
+}
+
+function disableLinks() {
+    allUserLinks.forEach(link =>{
+        link.classList.add('disabled-link');
+    });
 }
 
 
