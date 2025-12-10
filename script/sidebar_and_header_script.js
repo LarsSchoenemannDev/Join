eb48d055bbeacddbd37a4c64196c4a6bd45cf276
Dennis Köklü <dennis.koeklue@gmail.com>
2025-12-10 10:32:24 +0100

    changes on siderbar_and_header script for a If function

--- script/sidebar_and_header_script.js
+++ script/sidebar_and_header_script.js
@@ -1,9 +1,29 @@
 //Hier bitte alle globale Variablen setzen
+let menuButton = document.getElementById('menu-button');
 let user_icon = document.getElementById('user-icon');
 let switch_help = document.getElementById('switch_help');
-//const userStatus = sessionStorage.getItem('userStatus');
+const userName = sessionStorage.getItem('name');
+let initials = "";
 
+if (userName !== null && userName.trim() !== "") {
 
+    if (userName.length === 1) {
+    // Nur ein Name vorhanden
+    initials = userName[0][0].toUpperCase();
+    menuButtonName();
+} else {
+    // Mehrere Namen: erstes und letztes Wort
+    initials = userName[0][0].toUpperCase() + userName[userName.length - 1][0].toUpperCase();
+    menuButtonName();
+}
+
+} else{
+    menuButton.textContent = "";
+}
+
+function menuButtonName() {
+    menuButton.textContent = initials
+}
 
 function tooglemenu() {
     let msg = document.getElementById('dropDown-menu')
