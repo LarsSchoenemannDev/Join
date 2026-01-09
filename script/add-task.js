f42c93798291b3330c5b94a4129038dc391fedcb
LarsSchoenemannDev <lars.dev90@gmail.com>
2026-01-09 09:22:27 +0100

    rework assigned to rewook selected Elemente for the Inputs on the side rework validationRequired function add clean code

--- script/add-task.js
+++ script/add-task.js
@@ -1,63 +1,77 @@
 const BASE_URL = "https://joinproject-51c1f-default-rtdb.europe-west1.firebasedatabase.app/";
-let FetchData = [];
+let FetchData = {};
 let subTaskInput = [];
-let tempTaskData = [];
-processedContacts = [];
+let contactsState = [];
+let tempPageInputs = {};
+let taskData = {};
+
 const requiredFields = document.querySelectorAll("#title, #duedate, #category");
 const inputSubtask = document.getElementById("subtasks");
 const subtaskBox = document.getElementById("showHidden");
-const assignedTo = document.getElementById("BTNToggelContacts")
 
 async function init() {
     await getData();
+    renderContact()
     clearInputs();
     priorityMedium();
-    renderContact();
-    categorySelector();
 }
 
 async function getData() {
     try {
         const response = await fetch(BASE_URL + ".json");
         if (!response.ok) {
-            throw new Error(`HTTP error: ${response.status}`);
+            throw new Error(`HTTP error: ${response.status}`
+            );
         }
         const data = await response.json();
         FetchData = data;
+        contactsState = data.contacts.map(contact => ({
+            id: contact.id,
+            name: contact.name,
+            initials: contact.initials,
+            color: contact.color,
+            checked: false
+        }));
     } catch (error) {
         console.error("Fehler beim Laden der Daten:", error);
     }
 }
 
-function validateRequiredFields() {
-    const requiredFields = document.querySelectorAll("#title, #duedate, #category");
-    let requiredCheck = true;
-    const categoryValue = document.querySelector('input[name="priorityCategory"]:checked')?.value;
-    const categoryButton = document.getElementById("selectCategoryHtml");
-    requiredFields.forEach(required => {
-        if (required.value.trim() === "") {
-            required.classList.add("input-error");
-            requiredCheck = false;
-        } else {
-            required.classList.remove("input-error");
-        }
-    });
-    if (!categoryValue || categoryValue === "Select task category") {
-        categoryButton.classList.add("input-error");
-        requiredCheck = false;
-    } else {
-        categoryButton.classList.remove("input-error");
+function renderContact(list = contactsState) {
+    let html = "";
+    for (let i = 0; i < list.length; i++) {
+        const contact = list[i];
+        html += renderContactHTML(contact.initials, contact.name, contact.color, contact.id, contact.checked);
     }
-    return requiredCheck;
+    document.getElementById("selectContacts").innerHTML = html;
+}
+
+function assignedToLettersCheckContact() {
+    const container = document.getElementById("selectContact");
+    container.innerHTML = "";
+    contactsState.filter(c => c.checked).forEach(c => {
+        container.innerHTML += letterInitials(c);
+    });
+}
+
+function contactSearch() {
+    const value = document.getElementById("searchContacts").value.toLowerCase();
+    if (value.length === 0) {
+        renderContact();
+        return;
+    }
+
+    const filtered = contactsState.filter(c =>
+        c.name.toLowerCase().includes(value)
+    );
+    renderContact(filtered);
 }
 
 async function postAddTask(data) {
     try {
         const response = await fetch(BASE_URL, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
+            headers: { "Content-Type": "application/json", },
             body: JSON.stringify(data),
         });
         if (!response.ok) {
@@ -72,69 +86,31 @@ async function postAddTask(data) {
 }
 
 function priorityMedium() {
-    priorityMediumSelectet = document.querySelector('input[name="priority"]:checked')
+    let priorityMedium = document.querySelector('input[name="priority"]:checked')
 }
 
-function getSelectedContacts() {
-    return Array.from(
-        document.querySelectorAll(".checkbox-input:checked")
-    ).map(checkBox => checkBox.value);
-}
-
-function getSubtasks() {
-    return Array.from(
-        document.querySelectorAll("[data-index]")
-    ).map(subtaskElements => subtaskElements.value);
-}
-
-function toggelContacts() {
+function toggleContacts() {
+    document.getElementById("searchContacts").value = "";
     document.getElementById("selectContacts").classList.add("open");
-    document.getElementById("BTNToggelContacts").style.display = "none";
+    document.getElementById("BTNToggleContacts").style.display = "none";
     document.getElementById("searchContacts").classList.remove("d-none");
     document.getElementById("closeAssigned").style.display = "flex";
     document.getElementById("searchContacts").focus();
 }
 
+function toggleContact(id) {
+    const contact = contactsState.find(c => c.id === id);
+    contact.checked = !contact.checked;
+    assignedToLettersCheckContact();
+}
+
+
 function closeAssigned() {
     document.getElementById("selectContacts").classList.remove('open');
-    document.getElementById("BTNToggelContacts").style.display = 'flex';
+    document.getElementById("BTNToggleContacts").style.display = 'flex';
     document.getElementById("searchContacts").classList.add('d-none');
     document.getElementById("closeAssigned").style.display = "none";
     document.getElementById("searchContacts").value = "";
-}
-
-function renderContact() {
-    const contacts = FetchData.contacts;
-    let html = "";
-
-    for (let i = 0; i < contacts.length; i++) {
-        const contact = contacts[i];
-        processedContacts[i] = {
-            name: contact.name,
-            initials: contact.initials,
-            color: contact.color,
-            originalIndex: i
-        };
-        html += renderContactHTML(
-            contact.initials,
-            contact.name,
-            contact.color,
-            i
-        );
-    }
-    document.getElementById("selectContacts").innerHTML = html;
-}
-
-
-function assignedToLettersCheckContact() {
-    const container = document.getElementById("selectContact");
-    const allCheckbox = document.querySelectorAll(".checkbox-input:checked");
-    container.innerHTML = "";
-    allCheckbox.forEach(box => {
-        const i = box.getAttribute("data-index");
-        container.innerHTML += letterInitials(i);
-    })
-
 }
 
 function createSubtasks() {
@@ -149,15 +125,14 @@ function addSubtask() {
     let subTaskValue = subTask.value.trim()
     if (subTaskValue.length <= 3) return;
     subTaskInput.push(subTask.value);
-    renderSubtasks(subTaskValue);
+    renderSubtasks();
     subTask.value = ``;
-
     subTask.focus();
 }
 
 function renderSubtasks() {
     const subTaskContent = document.getElementById("SubtaskList");
-    let htmlContent = ``;
+    let htmlContent = "";
     for (let i = 0; i < subTaskInput.length; i++) {
         let subTaskNote = subTaskInput[i]
         htmlContent += subTaskContentHMTL(subTaskNote, i);
@@ -179,7 +154,6 @@ function saveSubtaskEdit(i) {
     subTaskInput[i] = subEditValue;
     renderSubtasks();
 }
-
 
 function deleteSubtask(i) {
     let focusinput = document.getElementById("subtasks")
@@ -217,10 +191,9 @@ requiredFields.forEach((field) => {
     field.addEventListener("blur", noneFocus);
 });
 
-
-function toggelCategory() {
-    const toggelCategory = document.getElementById("selectCategory");
-    toggelCategory.classList.toggle("open")
+function toggleCategory() {
+    const toggleCategory = document.getElementById("selectCategory");
+    toggleCategory.classList.toggle("open")
 }
 
 function categorySelector() {
@@ -237,12 +210,11 @@ function clearInputs() {
     document.getElementById("duedate").value = "";
     document.getElementById("prio-medium").checked = true;
     document.getElementById("SelectTaskCategory").checked = true;
-    categorySelector()    
+    categorySelector()
     document.getElementById("subtasks").value = "";
     document.getElementById("selectContact").innerHTML = "";
     document.querySelectorAll(".checkbox-input:checked").forEach((Checkbox) => { Checkbox.checked = false; });
 }
-
 
 document.addEventListener("DOMContentLoaded", function () {
     const subtaskElement = document.getElementById("subtasks");
@@ -261,6 +233,7 @@ document.addEventListener("DOMContentLoaded", function () {
 inputSubtask.addEventListener("focus", () => {
     subtaskBox.style.display = "flex";
 });
+
 document.addEventListener("click", (e) => {
     if (!e.target.closest(".input-wrapper")) {
         subtaskBox.style.display = "none";
@@ -269,25 +242,93 @@ document.addEventListener("click", (e) => {
 
 document.addEventListener("click", function (event) {
     const dropdown = document.getElementById("selectContacts");
-    const button = document.getElementById("BTNToggelContacts");
-
+    const button = document.getElementById("BTNToggleContacts");
     const clickedInsideDropdown = dropdown.contains(event.target);
     const clickedButton = button.contains(event.target);
-
     if (!clickedInsideDropdown && !clickedButton) {
-        closeAssigned();
     }
 });
 
 document.addEventListener("click", function (event) {
     const category = document.getElementById("selectCategory");
     const button = document.querySelector("#selectCategoryHtml button");
-
     const clickInsideCategory = category.contains(event.target);
     const clickOnButton = button.contains(event.target);
-
     if (!clickInsideCategory && !clickOnButton) {
         category.classList.remove("open");
     }
 });
 
+function getDataFromPage() {
+    let inputs = document.querySelectorAll("#title, #description, #duedate");
+    inputs.forEach(function (input) {
+        taskData[input.id] = input.value;
+    });
+    let selectedCategory = document.querySelector('input[name="priorityCategory"]:checked');
+    taskData.category = selectedCategory ? selectedCategory.value : "";
+    taskData.contacts = contactsState.filter(function (contact) {
+        return contact.checked;
+    });
+    taskData.subtasks = subTaskInput;
+    let priority = document.querySelector('input[name="priority"]:checked');
+    taskData.priority = priority ? priority.value : "medium";
+    console.table(taskData);
+    return taskData;
+}
+
+function validationRequired() {
+    const taskData = getDataFromPage();
+    const requiredFieldsValidation = ["title", "duedate", "category"];
+    let validationRequiredCheck = true;
+    requiredFieldsValidation.forEach((key) => {
+        const value = taskData[key];
+        const element = document.getElementById(key);
+        if (element) {
+            element.style.borderColor = "";
+        }
+        if (!value || value.trim() === "") {
+            console.warn("Validierung fehlgeschlagen:", key, "ist leer");
+            validationRequiredCheck = false;
+            if (element) {
+                element.style.borderColor = "#E60026";
+            }
+            return;
+        }
+        if (key === "category" && !["technical task", "user story"].includes(value.toLowerCase().trim())) {
+            console.warn("Validierung fehlgeschlagen: Kategorie nicht gewählt");
+            validationRequiredCheck = false;
+            if (element) {
+                element.style.borderColor = "#E60026";
+            }
+        }
+    });
+    return;
+}
+
+// async function putTasktooApi() {
+//     try {
+//         const response = await fetch(`${BASE_URL}/tasks.json`, {
+//             method: "PUT",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify(addTask)
+//         });
+
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+
+//         const result = await response.json();
+//         console.log('Tasks add updated:');
+//         return result;
+
+//     } catch (error) {
+//         console.error('Error updating contact:', error);
+//         throw error;
+//     }
+// }
+function pushTask() {
+    if (!validationRequired()) return;
+    getDataFromPage();
+}
--- script/add-taskHTML.js
+++ script/add-taskHTML.js
@@ -1,3 +1,63 @@
+function renderContactHTML(initials, name, color, id, checked) {
+    return `    
+    <ul class="contact-row">
+        <div class="contact-left">
+            <span class="initials-circle" style="background-color: ${color};">
+                ${initials}
+            </span>
+            <span class="contact-name">${name}</span>
+        </div>
+        <label class="checkbox">
+            <input class="checkbox-input" type="checkbox" ${checked ? "checked" : ""} onclick="toggleContact('${id}')">
+            <span class="checkbox-box"></span>
+        </label>
+    </ul>
+    `;
+}
+
+function renderContactSearchHTML(initials, name, color) {
+    return `    
+    <ul class="contact-row">
+        <div class="contact-left">
+            <span class="initials-circle" style="background-color: ${color};">${initials}</span>
+            <span class="contact-name">${name}</span>
+        </div>
+        <label class="checkbox">
+            <input id="checkbox" class="checkbox-input" name="checked" type="checkbox" value="">
+            <span class="checkbox-box"></span>
+        </label>
+    </ul>
+    `;
+}
+
+function letterInitials(contact) {
+    return `
+    <span class="initials-circle" style="background-color: ${contact.color}">
+        ${contact.initials}
+    </span>
+    `;
+}
+
+function setCategoryHTML(categoryLabel) {
+    return `    <button type="button" onclick="toggleCategory()" id="category"
+        class="input-styling input-icon-arrow-down custom-select-select input-Required">${categoryLabel}</button>             
+    `;
+}
+
+function subTaskContentHMTL(subTaskInput, i) {
+    return `
+    <div class="sub-container" data-index="${i}">
+        <span class="display-flex">&bull; ${subTaskInput}</span>
+        <div class="hover-show">
+            <img src="../assets/img/Subtasks change.svg" class="input-icon-cancel" onclick="changeSubtask(${i})">
+            <div class="seperator-small"></div>
+            <img src="../assets/img/SubTask delete.svg" class="input-icon-accept" onclick="deleteSubtask(${i})">
+        </div>
+    </div>
+    </div>
+    `;
+}
+
 function changeSubtaskHtml(i, currentValue) {
     return `<div class="input-wrapper"><input type="text" value="${currentValue}" id="edit-input-${i}"
     title="Enter the new text">
@@ -10,65 +70,12 @@ function changeSubtaskHtml(i, currentValue) {
     `;
 }
 
-function subTaskContentHMTL(subTaskInput, i) {
-    return `
-    <div class="sub-container" data-index="${i}">    
-    <span class="display-flex">&bull; ${subTaskInput}</span>  
-    <div class="hover-show">
-    <img src="../assets/img/Subtasks change.svg" class="input-icon-cancel" onclick="changeSubtask(${i})">
-    <div class="seperator-small"></div>
-    <img src="../assets/img/SubTask delete.svg" class="input-icon-accept" onclick="deleteSubtask(${i})">      
-    </div>   
-    </div> 
-    </div>
-    `;
-}
 
-function renderContactSearchHTML(initials, name, color) {
-    return `    
-    <ul class="contact-row">
-    <div class="contact-left">
-    <span class="initials-circle" style="background-color: ${color};">${initials}</span>
-    <span class="contact-name">${name}</span>
-    </div>
-    <label class="checkbox">
-    <input id="checkbox" class="checkbox-input" name="checked" type="checkbox" value="">
-    <span class="checkbox-box"></span>
-        </label>
-      </ul>
-    `;
-}
 
-function searchRenderHTMLZero() {
-    return `<div class="contactSearch"> no search found</div>`;
-}
 
-function renderContactHTML(initials, name, color, index) {
-    return `    
-    <ul class="contact-row">
-        <div class="contact-left">
-            <span class="initials-circle" style="background-color: ${color};">${initials}</span>
-            <span class="contact-name">${name}</span>
-        </div>
-        <label class="checkbox">
-            <input class="checkbox-input" type="checkbox" data-index="${index}" onclick="assignedToLettersCheckContact()">
-            <span class="checkbox-box" ></span>
-        </label>
-    </ul>
-    `;
-}
 
-function setCategoryHTML(categoryLabel) {
-    return `<button type="button" onclick="toggelCategory()" id="category"
-                class="input-styling input-icon-arrow-down custom-select-select input-Required">${categoryLabel}</button>                 
-`;
-}
 
-function letterInitials(i) {
-    return `
-        <span class="initials-circle"
-              style="background-color: ${processedContacts[i].color}">
-            ${processedContacts[i].initials}
-        </span>
-    `;
-}
+
+
+
+
