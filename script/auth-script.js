0c7811d849eaad806330cb048ab9aebe32c030a1
Niloufar <153923465+Niloufar88@users.noreply.github.com>
2026-02-05 15:49:46 +0330

    fixing an issue

--- script/auth-script.js
+++ script/auth-script.js
@@ -1,14 +1,10 @@
-
-
 window.onload = function () {
-  const userStatus = sessionStorage.getItem('userStatus');
-  const UID = sessionStorage.getItem('userID');
+  const userStatus = sessionStorage.getItem("userStatus");
+  const UID = sessionStorage.getItem("userID");
   //getData();
 
   // Wenn der Benutzer nicht eingeloggt ist.
-  if (!userStatus || (userStatus !== 'guest' && userStatus !== 'loggedIn')) {
-    window.location.href = "../html/login-site.html"; // Weiterleitung zur Login-Seite
+  if (!userStatus || (userStatus !== "guest" && userStatus !== "loggedIn")) {
+    window.location.href = "../index.html"; // Weiterleitung zur Login-Seite
   }
-}
-
-
+};
