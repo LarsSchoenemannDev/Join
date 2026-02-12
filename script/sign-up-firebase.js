2e1df51ddfd6911754da66f632e8cd2596d452ae
Niloufar <153923465+Niloufar88@users.noreply.github.com>
2026-02-13 00:26:57 +0330

    adding name and email validation functions by probability of checking the existing ones. form validation complete

--- script/sign-up-firebase.js
+++ script/sign-up-firebase.js
@@ -1,96 +1,4 @@
 /**
- * This script handles the user registration process by sending user data to a Firebase Realtime Database.
- */
-
-// let BASE_URL =
-//   "https://joinproject-51c1f-default-rtdb.europe-west1.firebasedatabase.app";
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const form = document.getElementById("signupForm");
-//   const nameInput = document.getElementById("name");
-//   const emailInput = document.getElementById("email");
-//   const passwordInput = document.getElementById("password");
-//   const confirmInput = document.getElementById("confirm-password");
-
-//   const errorBox = document.getElementById("error-message");
-//   const messageBox = document.getElementById("message");
-
-//   // SHA-256 Hash-Funktion
-//   async function hashPassword(password) {
-//     const encoder = new TextEncoder();
-//     const data = encoder.encode(password);
-
-//     const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-//     const hashArray = Array.from(new Uint8Array(hashBuffer));
-
-//     // in HEX umwandeln
-//     const hashHex = hashArray
-//       .map((b) => b.toString(16).padStart(2, "0"))
-//       .join("");
-
-//     return hashHex;
-//   }
-
-//   form.addEventListener("submit", async (e) => {
-//     e.preventDefault();
-
-//     errorBox.textContent = "";
-
-//     const trimName = nameInput.value.trim();
-//     const trimEmail = emailInput.value.trim();
-//     const trimPassword = passwordInput.value.trim();
-//     const confirm = confirmInput.value.trim();
-//     const hashedPassword = await hashPassword(trimPassword);
-
-//     // Passwort-Check
-//     if (trimPassword !== confirm) {
-//       errorBox.textContent = "Your passwords don't match. Please try again.";
-//       return;
-//     }
-
-//     const userData = {
-//       name: trimName,
-//       email: trimEmail,
-//       password: hashedPassword,
-//     };
-
-//     try {
-//       const response = await fetch(BASE_URL + "/users" + ".json", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(userData),
-//       });
-
-//       if (!response.ok) {
-//         console.error(`HTTP error! status: ${response.status}`);
-//       }
-
-//       const result = await response.json();
-//       console.log("User created:", result.name);
-
-//       // Erfolg anzeigen
-//       messageBox?.classList.add("show");
-//       showSuccessMessage();
-
-//       form.reset();
-//       button.disabled = true;
-//       UpdateIcon(passwordInput, iconPasswordDivMain);
-//       UpdateIcon(passwordConfirm, iconPasswordDivConfirm);
-
-//       setTimeout(() => {
-//         window.location.href = "../html/index.html";
-//       }, 2000);
-//     } catch (err) {
-//       console.error(err);
-//       errorBox.style.color = "red";
-//       errorBox.textContent = "Registration failed. Please try again.";
-//     }
-//   });
-// });
-
-/**
  * an async function to register a user by sending their data to the Firebase Realtime Database
  */
 const firebaseURL =
@@ -123,6 +31,68 @@ async function registerUser(name, email, password) {
     showErrorMessage("Registration failed. Please try again.");
   }
 }
+
+/**
+ * an async function to fetch existing user names from the Firebase Realtime Database and return them as an array. This function is used to check if a user name is already registered before allowing a new registration.
+ * @returns an Array with possible registered Names
+ */
+async function fetchExistingUserName() {
+  try {
+    const response = await fetch(firebaseURL + ".json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const existingNames = [];
+
+    if (data && typeof data === "object") {
+      for (const [id, userData] of Object.entries(data)) {
+        if (userData.name) {
+          existingNames.push(userData.name);
+        }
+      }
+      console.log("Fetched names:", existingNames);
+      return existingNames;
+    }
+  } catch (error) {
+    console.error(error);
+    console.log("fetching failed.");
+    return [];
+  }
+}
+
+/**
+ * an async function to fetch existing user emails from the Firebase Realtime Database and return them as an array. This function is used to check if an email is already registered before allowing a new registration.
+ * @returns an Array of possible registered Emails
+ */
+async function fetchExistingUserEmail() {
+  try {
+    const response = await fetch(firebaseURL + ".json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const existingEmails = [];
+
+    if (data && typeof data === "object") {
+      for (const [id, userData] of Object.entries(data)) {
+        if (userData.email) {
+          existingEmails.push(userData.email);
+        }
+      }
+      console.log("Fetched emails:", existingEmails);
+      return existingEmails;
+    }
+  } catch (error) {
+    console.error(error);
+    console.log("fetching failed.");
+    return [];
+  }
+}
+fetchExistingUserEmail();
+fetchExistingUserName();
 
 /**
  * Function to show a success message after signing up
